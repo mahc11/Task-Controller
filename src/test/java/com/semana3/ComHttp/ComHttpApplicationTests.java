1cedda85ece03e8fe3c9f5450e2028ab51672c65
mahc11 <mianheca.12@gmail.com>
2022-10-25 00:16:29 -0500

    Corrigiendo todos los errores.

--- src/test/java/com/semana3/ComHttp/ComHttpApplicationTests.java
+++ src/test/java/com/semana3/ComHttp/ComHttpApplicationTests.java
@@ -1,7 +1,7 @@
 package com.semana3.ComHttp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class ComHttpApplicationTests {
